1562856507545faa41391f00e7e84c79f3c49ba0
GuiTavs7 <7tavs7@gmail.com>
2024-05-12 21:16:55 -0300

    upload

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+/* 
+
 window.alert("Hello World!")
 window.confirm("Deseja continuar?")
 window.prompt("Digite sua idade")
 
 console.log("Olá!")
+
+*/
+
+// ARMAZENANDO INTERAÇÃO DO USUÁRIO (INPUTS E ENTRADAS) EM UMA VARIÁVEL
+
+let retornoDoUsuario = confirm("Deseja continuar?")
+
+console.log(retornoDoUsuario)
+
+// OK = TRUE
+// CANCEL = FALSE
+
+let idade = prompt("Digite sua idade")
+
+console.log(idade)
